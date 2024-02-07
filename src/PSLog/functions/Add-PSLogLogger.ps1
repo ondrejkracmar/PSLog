ee019fad897860bd5e7c644667bd61d0386eda1d
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2024-02-07 17:42:45 +0100

    Test

--- src/PSLog/functions/Add-PSLogLogger.ps1
+++ src/PSLog/functions/Add-PSLogLogger.ps1
@@ -12,24 +12,6 @@
 	.PARAMETER LoggerProvider
 		Logger provider type 'ConsoleLogger', 'TextFileLogger', 'ApplicationInsightsLogger', 'EmailLogger'
 
-	.PARAMETER AdditionalDataProvider
-		Add additiobal data provider
-
-	.PARAMETER EnableException
-        This parameters disables group-friendly warnings and enables the throwing of exceptions. This is less group frien
-        dly, but allows catching exceptions in calling scripts.
-
-	.PARAMETER WhatIf
-        Enables the function to simulate what it will do instead of actually executing.
-
-    .PARAMETER Confirm
-        The Confirm switch instructs the command to which it is applied to stop processing before any changes are made.
-        The command then prompts you to acknowledge each action before it continues.
-        When you use the Confirm switch, you can step through changes to objects to make sure that changes are made only to the specific objects that you want to change.
-        This functionality is useful when you apply changes to many objects and want precise control over the operation of the Shell.
-        A confirmation prompt is displayed for each object before the Shell modifies the object.
-
-
 	.EXAMPLE
 		PS C:\> $PSLogger = Add-PSLogLogger -DateTimeNowProvider UtcDateTimeProvider -LoggerProvider ConsoleLogger -AdditionalDataProvider $AdditionalDataProvider
 
