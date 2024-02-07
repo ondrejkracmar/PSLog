b7f8b8f4cca2f0bd25db93c4e498720a9ab45fcd
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2024-02-07 17:15:41 +0100

    Test

--- src/PSLog/functions/Write-PSLogWarning.ps1
+++ src/PSLog/functions/Write-PSLogWarning.ps1
@@ -13,16 +13,6 @@ Function Write-PSLogWarning {
 	.PARAMETER Message
 		Log warning message
 
-	.PARAMETER WhatIf
-        Enables the function to simulate what it will do instead of actually executing.
-
-    .PARAMETER Confirm
-        The Confirm switch instructs the command to which it is applied to stop processing before any changes are made.
-        The command then prompts you to acknowledge each action before it continues.
-        When you use the Confirm switch, you can step through changes to objects to make sure that changes are made only to the specific objects that you want to change.
-        This functionality is useful when you apply changes to many objects and want precise control over the operation of the Shell.
-        A confirmation prompt is displayed for each object before the Shell modifies the object.
-
 	.INPUTS
 		Isystem.Infrastructure.Core.ILogger[]]. Pipe objects.
 
@@ -63,10 +53,8 @@ Function Write-PSLogWarning {
 
 	Process {
 		foreach ($itemLogProvider in $LoggerProvider) {
-			if ($PSCmdlet.ShouldProcess(($itemLogProvider.GetType()).name, (Get-PSFLocalizedString -Module $script:ModuleName -Name LoggerProvider.WriteWarning))) {
-				$itemLogProvider.LogWarning($Message)
-				[void]$listLoggerProviders.Add($itemLogProvider)
-			}
+			$itemLogProvider.LogWarning($Message)
+			[void]$listLoggerProviders.Add($itemLogProvider)
 		}
 	}
 
